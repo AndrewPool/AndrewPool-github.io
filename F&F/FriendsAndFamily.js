ac1cc5c50e0ea8bd599f60ddff3787a539faae8e
Andrew Pool <andrewpooljob@gmail.com>
2020-02-07 17:10:14 -0700

    Update FriendsAndFamily.js

--- F&F/FriendsAndFamily.js
+++ F&F/FriendsAndFamily.js
@@ -90,9 +90,9 @@ function wrtJSON() {
 
             for(var i = 0; i< pets.length; i++){
               var pet = pets[i];
-              if(pet.name.toLowerCase().indexOf(relationInputText)>-1 && nameInputText != "" && pet.relation.toLowerCase().indexOf(relationInputText)>-1){//has name and relation add
+              if(pet.name.toLowerCase().indexOf(relationInputText)>-1 && nameInputText != "" && (pet.relation.toLowerCase().indexOf(relationInputText)>-1 || relationInputText == "")){//has name and relation add
                   itemsInList += contentForEntry(pet);
-              }else if (pet.lname.toLowerCase().indexOf(relationInputText)>-1  && nameInputText != "" && pet.relation.toLowerCase().indexOf(relationInputText)>-1) {//has last name relation add
+              }else if (pet.lname.toLowerCase().indexOf(relationInputText)>-1  && nameInputText != "" && (pet.relation.toLowerCase().indexOf(relationInputText)>-1 || relationInputText == "")) {//has last name relation add
                   itemsInList += contentForEntry(pet);
               }else if(pet.relation.toLowerCase().indexOf(relationInputText)>-1  && relationInputText != ""){//has relationship add
                   itemsInList += contentForEntry(pet);
